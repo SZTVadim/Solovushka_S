2298436693977ba1a182fb90de941eb72903be85
solovyovasa <msssoloveva26@gmail.com>
2026-02-07 17:15:18 +0300

    дз 20-1

--- homework/lesson_20.py
+++ homework/lesson_20.py
@@ -11,10 +11,16 @@
 # Выполнить запрос Delete
 
 import requests
-response = requests.post( "https://petstore.swagger.io/v2/pet"),
-headers = {"accept": "application/json", "Content-Type": "application/json"},
-data_for_post = {
-  "id": 9223372036854776000,
+
+
+# POST
+
+url_post = "https://petstore.swagger.io/v2/pet"
+
+headers_post = {"accept": "application/json", "Content-Type": "application/json"}
+
+data_post = {
+  "id": 669,
   "category": {
     "id": 0,
     "name": "string"
@@ -31,6 +37,61 @@ data_for_post = {
   ],
   "status": "available"
 }
-response = requests.post("https://petstore.swagger.io/v2/pet", headers=headers, json=data_for_post)
-print(f"{response.status_code}, успешно создан")
-print(response.json())
+
+response_post = requests.post(url_post, json=data_post, headers=headers_post)
+
+print(f" Успешно создан  Status code : {response_post.status_code}")
+print(f" Тело ответа POST: {response_post.json()}")
+
+
+# GET
+
+url_get = "https://petstore.swagger.io/v2/pet/669"
+
+headers_get = {"accept": "application/json"}
+response_get = requests.get(url_get, headers=headers_get)
+print(f"Успешно получен  Status code : {response_get.status_code}")
+print(f" Тело ответа GET: {response_get.json()}")
+
+
+# PUT
+
+
+url_put = "https://petstore.swagger.io/v2/pet"
+
+headers_put = {"accept": "application/json", "Content-Type": "application/json"}
+
+data_put = {
+  "id": 669,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "doggie DOG",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+}
+
+response_put = requests.put(url_put, json=data_put, headers=headers_put)
+
+print(f" Успешно обновлен  Status code : {response_put.status_code}")
+print(f" Тело ответа PUT: {response_put.json()}")
+
+
+# DELETE
+
+
+url_delete = "https://petstore.swagger.io/v2/pet/669"
+
+headers_delete = {"accept": "application/json"}
+response_delete = requests.delete(url_delete, headers=headers_delete)
+print(f"Успешно удален  Status code : {response_delete.status_code}")
+print(f" Тело ответа DELETE: {response_delete.json()}")
